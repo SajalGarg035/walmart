a736383ad49a200620239f3eca5d9a524c5b9409
Sajal Garg <164674816+SajalGarg035@users.noreply.github.com>
2025-07-14 11:00:45 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
-import { CartProvider } from './contexts/CartContext';
+
 import { SocketProvider } from './contexts/SocketContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
