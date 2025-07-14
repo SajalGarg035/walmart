8a3c4411dcacddb193d5d2a1e1c0687a1327eb2a
Sajal Garg <164674816+SajalGarg035@users.noreply.github.com>
2025-07-14 11:04:03 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,7 @@ function App() {
               <Footer />
               <Toaster position="top-right" />
             </div>
+     </div>
           </Router>
         </SocketProvider>
       </CartProvider>
